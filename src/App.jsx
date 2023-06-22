0175b9883fa748cc8403c1f4b8d93ba425856e22
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-06-22 09:49:19 +0530

    Added Router , Routes and Route from react router to route two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
+import {Link, Routes, Route} from "react-router-dom"
+
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <h1>Code Goes Here</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />}/>
+      </Routes>
     </div>
   );
 }
